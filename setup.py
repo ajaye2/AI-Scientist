4a8a047c4c92595984db75a0af910a0b62b781a7
Abubakarr Jaye <ajaye2@illinois.edu>
2024-12-07 15:44:37 -0500

    Update version to 0.1.5 in setup.py to reflect the latest changes in package configuration.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ai-scientist",
-    version="0.1.4",
+    version="0.1.5",
     author="",  # TODO: Add your name
     author_email="",  # TODO: Add your email
     description="An AI-powered scientific research assistant",
