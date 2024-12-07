404c6ff222427bc5241bfef3c66e97e34142d607
Abubakarr Jaye <ajaye2@illinois.edu>
2024-12-07 15:17:42 -0500

    Update version to 0.1.3 in setup.py and adjust included PDF files for package distribution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ai-scientist",
-    version="0.1.2",
+    version="0.1.3",
     author="",  # TODO: Add your name
     author_email="",  # TODO: Add your email
     description="An AI-powered scientific research assistant",
@@ -52,6 +52,7 @@ setup(
             "templates/*/*.pdf", 
             "ai_scientist/fewshot_examples/*",
             "fewshot_examples/*.pdf",
+            # "ai_scientist/fewshot_examples/*.pdf",
         ],
     },
     include_package_data=True,
