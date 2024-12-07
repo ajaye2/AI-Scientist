54ce0f2b021d5659d342f33de005323a87c06cce
Abubakarr Jaye <ajaye2@illinois.edu>
2024-12-07 15:08:28 -0500

    Update version to 0.1.1 in setup.py and add __init__.py for fewshot_examples module

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ai-scientist",
-    version="0.1.0",
+    version="0.1.1",
     author="",  # TODO: Add your name
     author_email="",  # TODO: Add your email
     description="An AI-powered scientific research assistant",
